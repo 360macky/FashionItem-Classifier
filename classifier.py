c248dac3f2fab180e616b8c37529cd23ad90bc0f
Marcelo Arias <mail.marcelo.as@gmail.com>
2019-12-16 11:07:17 -0500

    Classifier in Python created

---
+++ classifier.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+from tensorflow import keras
+import matplotlib.pyplot as plt
+
+data = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+class_names = ['T-shirt/top',
+               'Trouser',
+               'Pullover',
+               'Dress',
+               'Coat',
+               'Sandal',
+               'Shirt',
+               'Sneaker',
+               'Bag',
+               'Ankle boot']
+
+# Neural Network Development
+model = keras.Sequential([
+                          keras.layers.Flatten(input_shape=(28, 28)),
+                          keras.layers.Dense(128, activation="relu"),
+                          keras.layers.Dense(10, activation="softmax")
+                          ])
+
+model.compile(optimizer="adam",
+              loss="sparse_categorical_crossentropy",
+              metrics=["accuracy"])
+
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print("Test accuracy: ", test_acc)
+
+# Prediction of the model
+prediction = model.predict(test_images)
+
+for i in range(7):
+  plt.grid(False)
+  plt.imshow(test_images[i], cmap=plt.cm.binary)
+  plt.xlabel("Actual: "  + class_names[test_labels[i]])
+  plt.title("Prediction: "  + class_names[np.argmax(prediction[i])])
+  plt.show()
